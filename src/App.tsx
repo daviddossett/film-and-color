48a9b00dc309e0e52da0b6065cd3a9af28045d10
David Dossett <daviddossett@outlook.com>
2020-04-30 19:51:51 -0700

    Refactored tree and added logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
 import React from 'react';
-import Clarifai from 'clarifai';
 
 import './App.css';
-import { HeroImage } from './components/HeroImage/HeroImage';
-import { PaletteView } from './components/PaletteView/PaletteView';
-
-const selectedImage = 'https://wallpapercave.com/wp/wp4628469.jpg';
-const modelId = 'eeed0b6733a644cea07cf4c60f87ebb7';
-
-const app = new Clarifai.App({
-  apiKey: '50813a3a6f6a447d9d2853ebba96a18b'
- });
-
-async function getPalette(model: string, image: string): Promise<String> {
-  let response = await app.models.predict(model, image);
-  let colors = await response.outputs[0].data.colors;
-  const generatedPalette: any = [];
-  
-  for (let i = 0; i < colors.length; i++) {
-    generatedPalette.push(colors[i].raw_hex);
-  }
-  return generatedPalette;
-}
-
-const palette = getPalette(modelId, selectedImage);
+import { RootPage } from './components/RootPage/RootPage';
 
 function App() {
   return (
-    <div className={'wrapper'}>
-      <HeroImage />
-      <PaletteView palette={palette} />
-    </div>
+    <>
+      <RootPage />
+    </>
   );
 }
 
---
+++ src/components/RootPage/RootPage.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import Clarifai from 'clarifai';
+
+import './RootPage.css';
+import { HeroImage } from '../HeroImage/HeroImage';
+import { PaletteView } from '../PaletteView/PaletteView';
+
+interface RootPageProps {}
+
+const selectedImage = 'https://wallpapercave.com/wp/wp3535960.jpg';
+const modelId = 'eeed0b6733a644cea07cf4c60f87ebb7';
+
+const app = new Clarifai.App({
+  apiKey: '50813a3a6f6a447d9d2853ebba96a18b'
+});
+
+async function getPalette() {
+   let response = await app.models.predict(modelId, selectedImage);
+   let colors = response.outputs[0].data.colors;
+   let palette = [];
+   
+   for (let i = 0; i < colors.length; i++) {
+       palette.push(colors[i].raw_hex)
+   }
+   console.log(palette);
+   return palette;
+}
+
+export const RootPage: React.FC<RootPageProps> = () => {
+  const [palette, setPalette] = useState([]);
+
+  useEffect(() => {
+      function handlePaletteUpdate(somePalette: any) {
+        setPalette(somePalette);
+      }
+
+      getPalette().then(function(response) {
+          handlePaletteUpdate(response);
+      });
+  }, [])
+
+  return (
+    <div className={'wrapper'}>
+      <HeroImage />
+      <PaletteView palette={palette} />
+    </div> 
+  );
+}
