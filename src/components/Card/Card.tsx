61068f793985610cb76b395d71ec93505922d95c
David Dossett <daviddossett@outlook.com>
2020-05-05 21:48:44 -0700

    Added Card and Feed components

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface CardProps {
+  image: string;
+}
+
+export const Feed: React.FC<CardProps> = () => {
+  return <></>;
+};
---
+++ src/components/Feed/Feed.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface FeedProps {
+  images: string[];
+}
+
+export const Feed: React.FC<FeedProps> = () => {
+  return <></>;
+};
