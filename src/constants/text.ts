60a920f8f2c8b9469f9214b31158ec1da721b6be
David Dossett <daviddossett@outlook.com>
2020-05-06 21:30:24 -0700

    Added text objects

--- src/constants/text.ts
+++ src/constants/text.ts
@@ -4,17 +4,58 @@ export const text = {
   header32: {
     fontSize: '32px',
     fontWeight: 600,
-    color: colors.textPrimary,
+    lineHeight: '32px',
+  },
+  header28: {
+    fontSize: '28px',
+    fontWeight: 600,
+    lineHeight: '28px',
+  },
+  header24: {
+    fontSize: '24px',
+    fontWeight: 500,
+    lineHeight: '24px',
+  },
+  header20: {
+    fontSize: '20px',
+    fontWeight: 500,
+    lineHeight: '20px',
   },
   header14: {
     fontSize: '14px',
     fontWeight: 500,
     textTransform: 'uppercase',
     letterSpacing: 0.2,
+    lineHeight: '14px',
+  },
+  paragraphPrimary: {
+    fontSize: '16px',
+    fontWeight: 400,
+    lineHeight: '24px',
+  },
+  paragraphSecondary: {
+    fontSize: '16px',
+    fontWeight: 500,
+    lineHeight: '24px',
   },
   bodyPrimary: {
     fontSize: '14px',
     fontWeight: 400,
-    color: colors.textPrimary,
+    lineHeight: '22px',
+  },
+  bodySecondary: {
+    fontSize: '14px',
+    fontWeight: 500,
+    lineHeight: '22px',
+  },
+  captionPrimary: {
+    fontSize: '12px',
+    fontWeight: 400,
+    lineHeight: '18px',
+  },
+  captionSecondary: {
+    fontSize: '12px',
+    fontWeight: 500,
+    lineHeight: '18px',
   },
 };
