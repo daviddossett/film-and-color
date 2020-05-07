a0a2e2c6f60a16c230f4914444fe010d3fa26350
David Dossett <daviddossett@outlook.com>
2020-05-06 21:19:27 -0700

    Removed old styles

---
+++ src/constants/text.ts
@@ -0,0 +1,20 @@
+import { colors } from './colors';
+
+export const text = {
+  header32: {
+    fontSize: '32px',
+    fontWeight: 600,
+    color: colors.textPrimary,
+  },
+  header14: {
+    fontSize: '14px',
+    fontWeight: 500,
+    textTransform: 'uppercase',
+    letterSpacing: 0.2,
+  },
+  bodyPrimary: {
+    fontSize: '14px',
+    fontWeight: 400,
+    color: colors.textPrimary,
+  },
+};
