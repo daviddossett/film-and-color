337c7d0bbdf1032b6e87a57585c230405f5e86b9
David Dossett <daviddossett@outlook.com>
2020-09-15 21:08:11 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d82f1a08b574c9dc7f88f2f6683670cf.js"
+  "/precache-manifest.1d9121eb0a1425e2110525d2292c1fb7.js"
 );
 
 self.addEventListener('message', (event) => {
