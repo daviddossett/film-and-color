bfebf4d8438c32099a1d663e869088a9885abb58
David Dossett <daviddossett@outlook.com>
2020-09-16 17:35:20 -0700

    More layout updates

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -13,6 +13,7 @@ function App() {
 
   const { documents } = useFirestore('images');
   const [image, setImage] = useState('');
+  const date = new Date().toDateString();
 
   useEffect(() => {
     if (documents.length > 0) {
@@ -23,10 +24,16 @@ function App() {
   return (
     <div className={'grid-container'}>
       <div className={'grid'}>
-        <TitleBar className={'titlebar'} />
+        <h1 className={'header'}>Film and Color</h1>
+        <div className={'info'}>
+          <p>{date}</p>
+          <p>The Grand Budapest Hotel</p>
+          <p>Art direction by Stephan Gellar</p>
+        </div>
         <Palette className={'palette'} image={image} />
         <HeroImage className={'image'} imageUrl={image} />
         <Footer className={'footer'} />
+        <Footer className={'footer2'} />
       </div>
     </div>
   );
--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -12,7 +12,6 @@ export const TitleBar: React.FC<TitleBarProps> = ({ className }) => {
   return (
     <div className={`${className} title-bar-root`}>
       <h1 className={'title'}>Film and Color</h1>
-      <span className={'date'}>{date}</span>
     </div>
   );
 };
