2eb3dce56e4c21c9f35fa5bdb93ddbb11b64c4ab
mistople <ddossett@microsoft.com>
2021-02-09 21:13:42 -0800

    Added hover event

--- src/components/Swatch/Swatch.tsx
+++ src/components/Swatch/Swatch.tsx
@@ -7,16 +7,27 @@ export interface SwatchProps {
   className?: string;
 }
 
+declare global {
+  interface Window { analytics: any; }
+}
+
+
 export const Swatch: React.FC<SwatchProps> = ({ color, isLoading }) => {
   const style = {
     backgroundColor: color,
   };
 
+  function handleSwatchHover(): any {
+    window.analytics.track("Swatch hovered", {
+      hexValue: color,
+    });
+  }
+
   if (isLoading) return <div className={'loadingSwatch'} />;
 
   return <div style={style} className={'swatchWrapper'}>
     <div className={'hex-wrapper'}>
-      <p className={'hex'}>{color}</p>
+      <p onMouseOver={handleSwatchHover}className={'hex'}>{color}</p>
     </div>
   </div>;
 };
